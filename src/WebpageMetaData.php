34215177bb549200b5f0783c988dc242a38cfced
Jaimeson Waugh <jaimeson@4cesi.com>
2019-08-12 15:41:25 -0700

    Finish initial version

--- src/WebpageMetaData.php
+++ src/WebpageMetaData.php
@@ -3,9 +3,39 @@
 declare( strict_types = 1 );
 namespace WaughJ\WebpageMetaData;
 
+use Symfony\Component\DomCrawler\Crawler;
+
 class WebpageMetaData
 {
-	public function __construct()
+	public function __construct( string $html )
 	{
+		$crawler = new Crawler( $html );
+		$title_element = $crawler->filter( 'title' );
+		$this->title = ( $title_element->count() > 0 ) ? $title_element->first()->text() : null;
+		$desc_element = $crawler->filter( 'meta[name="description"]' );
+		$this->description = ( $desc_element->count() > 0 ) ? $desc_element->first()->attr( "content" ) : null;
 	}
+
+	public function hasTitle() : bool
+	{
+		return $this->title !== null;
+	}
+
+	public function hasDescription() : bool
+	{
+		return $this->description !== null;
+	}
+
+	public function getTitle() : string
+	{
+		return ( $this->hasTitle() ) ? $this->title : '';
+	}
+
+	public function getDescription() : string
+	{
+		return ( $this->hasDescription() ) ? $this->description : '';
+	}
+
+	private $title;
+	private $description;
 }
--- tests/WebpageMetaDataTest.php
+++ tests/WebpageMetaDataTest.php
@@ -7,9 +7,108 @@ use WaughJ\WebpageMetaData\WebpageMetaData;
 
 class WebpageMetaDataTest extends TestCase
 {
-	public function testObjectWorks() : void
+	public function testMissingTags() : void
 	{
-		$object = new WebpageMetaData();
-		$this->assertTrue( is_object( $object ) );
+		$html =
+		'
+			<!DOCTYPE html>
+			<html>
+			</html>
+		';
+		$meta_data = new WebpageMetaData( $html );
+		$this->assertFalse( $meta_data->hasTitle() );
+		$this->assertFalse( $meta_data->hasDescription() );
+		$this->assertEquals( '', $meta_data->getTitle() );
+		$this->assertEquals( '', $meta_data->getDescription() );
+	}
+
+	public function testWorks() : void
+	{
+		$html =
+		'
+			<!DOCTYPE html>
+			<html>
+				<head>
+					<title>I am the Meta Title</title>
+					<meta name="description" content="& I am the Meta Description." />
+				</head>
+			</html>
+		';
+		$meta_data = new WebpageMetaData( $html );
+		$this->assertTrue( $meta_data->hasTitle() );
+		$this->assertTrue( $meta_data->hasDescription() );
+		$this->assertEquals( 'I am the Meta Title', $meta_data->getTitle() );
+		$this->assertEquals( '& I am the Meta Description.', $meta_data->getDescription() );
+	}
+
+	public function testEmpty() : void
+	{
+		$html =
+		'
+			<!DOCTYPE html>
+			<html>
+				<head>
+					<title></title>
+					<meta name="description" content="" />
+				</head>
+			</html>
+		';
+		$meta_data = new WebpageMetaData( $html );
+		$this->assertTrue( $meta_data->hasTitle() );
+		$this->assertTrue( $meta_data->hasDescription() );
+		$this->assertEquals( '', $meta_data->getTitle() );
+		$this->assertEquals( '', $meta_data->getDescription() );
+	}
+
+	public function testEmptyNoDescContent() : void
+	{
+		$html =
+		'
+			<!DOCTYPE html>
+			<html>
+				<head>
+					<title></title>
+					<meta name="description" />
+				</head>
+			</html>
+		';
+		$meta_data = new WebpageMetaData( $html );
+		$this->assertTrue( $meta_data->hasTitle() );
+		$this->assertFalse( $meta_data->hasDescription() );
+		$this->assertEquals( '', $meta_data->getTitle() );
+		$this->assertEquals( '', $meta_data->getDescription() );
+	}
+
+	public function testInvalidHTML() : void
+	{
+		$html = 'asfsdfsf';
+		$meta_data = new WebpageMetaData( $html );
+		$this->assertFalse( $meta_data->hasTitle() );
+		$this->assertFalse( $meta_data->hasDescription() );
+		$this->assertEquals( '', $meta_data->getTitle() );
+		$this->assertEquals( '', $meta_data->getDescription() );
+	}
+
+	public function testMultiple() : void
+	{
+		$html =
+		'
+			<!DOCTYPE html>
+			<html>
+				<head>
+					<title>I am the Meta Title</title>
+					<meta name="description" content="& I am the Meta Description." />
+					<title>I am the 2nd Meta Title</title>
+					<meta name="description" content="& I am the 2nd Meta Description." />
+				</head>
+				<meta name="description" content="& I am the 3rd Meta Description." />
+				<title>I am the 3rd Meta Title</title>
+			</html>
+		';
+		$meta_data = new WebpageMetaData( $html );
+		$this->assertTrue( $meta_data->hasTitle() );
+		$this->assertTrue( $meta_data->hasDescription() );
+		$this->assertEquals( 'I am the Meta Title', $meta_data->getTitle() );
+		$this->assertEquals( '& I am the Meta Description.', $meta_data->getDescription() );
 	}
 }
